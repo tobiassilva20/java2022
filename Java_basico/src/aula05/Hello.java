cee6d154e6693542139b21f31e16cca08297a98d
Tobias Silva <tobiassilva@hotmail.it>
2022-04-05 13:08:31 -0300

    aula 08

--- Java_basico/src/aula05/Hello.java
+++ Java_basico/src/aula05/Hello.java
@@ -1,7 +1,12 @@
 package aula05;
+// Nesta aula foi ensinado como criar um programa simples em Java
 
+// Classe principal do programa
 public class Hello {
+
+    // Método main. O ponto de início de um programa Java
     public static void main(String[] args) {
+        // Exibe o texto Olá Java 2022 no console.
         System.out.println("Olá Java 2022!");
     }
 }
