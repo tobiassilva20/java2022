a9e74460ab5492ba3718973c38db5a550d3397fc
Tobias Silva <tobiassilva@hotmail.it>
2022-04-05 13:20:45 -0300

    aula 10

---
+++ Java_basico/src/aula10/Variaveis.java
@@ -0,0 +1,12 @@
+package aula10;
+// Nesta aula foi feita uma introdução sobre variáveis.
+public class Variaveis {
+    public static void main(String[] args) {
+        // Tipos primitivos
+        int idade  = 20;
+        String nome = "JAVA";
+       boolean falsoOuVerdadeiro = true;
+        float salario = 2.500f;
+        System.out.println(nome + " " + idade + " " + falsoOuVerdadeiro + " " + salario);
+    }
+}
