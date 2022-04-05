8bee6e4fb2e97918264f4c09daf4404cb999d5e5
Tobias Silva <tobiassilva@hotmail.it>
2022-04-05 12:42:35 -0300

    aula 06

---
+++ Java_basico/src/aula06/Argumentos.java
@@ -0,0 +1,12 @@
+package aula06;
+
+// Nesta aula foi explicado como passar argumentos para os programas
+public class Argumentos {
+
+    // Os argumentos podem ser passados para o programa através do array de strings args
+    public static void main(String[] args) {
+
+        // Acessando o primeiro elemento do array (args[0])
+        System.out.println("Argumento passado: " + args[0]);
+    }
+}
