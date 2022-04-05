b75444404e6725fce9a89370231456346c436204
Tobias Silva <tobiassilva@hotmail.it>
2022-04-05 13:39:13 -0300

    aula 11

---
+++ Java_basico/src/aula11/VariaveisInteiras.java
@@ -0,0 +1,23 @@
+package aula11;
+// Nesta aula foi dado continuidade ao estudos das variáveis no Java
+
+public class VariaveisInteiras {
+
+
+    public static void main(String[] args) {
+        // Tipos de variáveis numéricas inteiras
+
+        byte numero_byte = 5;
+
+        short numero_short = 10;
+
+        int numero_int = 2000;
+
+        long numero_long = 255555555;
+
+        System.out.println(numero_byte + " byte");
+        System.out.println(numero_short + "short");
+        System.out.println(numero_int + "inteiro");
+        System.out.println(numero_long + "long");
+    }
+}
