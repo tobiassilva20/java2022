49c202e11ed7dd4d8dde734828c3bd4a7f79d704
Tobias Silva <tobiassilva@hotmail.it>
2022-04-12 10:42:48 -0300

    aula 21

---
+++ Java_basico/src/aula21/ForEach.java
@@ -0,0 +1,17 @@
+package aula21;
+
+// Nesta aula foi ensinado o uso do laço foreach
+public class ForEach {
+    public static void main(String[] args) {
+        int[] notas = new int[5];
+        for (int i=0, max = notas.length; i < max; i++){
+            notas[i] = i + 2;
+        }
+
+        // Iterando com foreach
+        for (int nota: notas) {
+            System.out.println(nota);
+        }
+
+    }
+}
