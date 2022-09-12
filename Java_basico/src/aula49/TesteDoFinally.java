fdc3d381a43319effde81b1c16ff80fa70d87c85
Tobias Silva <tobiassilva@hotmail.it>
2022-09-12 11:29:14 -0300

    Bloco try-catch-finally

--- Java_basico/src/aula49/TesteDoFinally.java
+++ Java_basico/src/aula49/TesteDoFinally.java
@@ -1,7 +1,8 @@
-package aula48;
+package aula49;
 
-// Nesta aula foi mostrado como é possível realizar a captura de múltiplas exceções no Java
-public class MultiplosCatches {
+
+// Nesta aula foram explicados os casos em que o bloco finally é utilizado e quando ele é executado ou não
+public class TesteDoFinally {
     public static void main(String[] args) {
         int[] numeros = {4, 8, 16, 32, 64, 128};
         int[] denominadores = {2, 0, 4, 8, 0};
@@ -11,8 +12,13 @@ public class MultiplosCatches {
                 System.out.println(numeros[i] + "/" + denominadores[i] + "=" + (numeros[i]/denominadores[i]));
             } catch (ArithmeticException e0){ // Exceção de um tipo
                 System.out.println("Divisão por 0 inválida!");
+                // System.exit(0);
+                // Caso a linha acima seja descomentada, sempre que um erro for capturado a execução do
+                // programa finaliza.
             } catch (ArrayIndexOutOfBoundsException e1) { // Exceção de outro tipo em um catch separado.
                 System.out.println("Divisão inválida!");
+            }finally {
+                System.out.println("Sempre sera impressa");
             }
 
             // Outra forma possível de realizar tratamento de múltiplos erros.
